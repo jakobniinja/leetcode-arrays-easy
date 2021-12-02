f074ff0a0d27b2ff4812811df8649010f1ed6afd
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-02 22:29:26 +0100

    impl mergeSortedArray

---
+++ src/arrays/MergeSortedArray.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public  static void mergeSortedArray( int[]  nums1, int m, int[]  nums2, int n) {
+
+        for (int i = 0; i < n; i++) {
+           nums1[m+i] = nums2[i];
+        }
+           Arrays.sort(nums1);
+    }
+
+}
