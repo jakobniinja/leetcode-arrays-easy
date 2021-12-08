71fcd89f6add7f49dc9c5586d806ec6dbb33c6df
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-08 21:49:34 +0100

    impl thirdMax

---
+++ src/arrays/ThirdMax.java
@@ -0,0 +1,11 @@
+package arrays;
+
+
+public class ThirdMax {
+    public  static int thirdMax(int[] nums) {
+        Array.sort(nums);
+        int n = nums.length -1 ;
+        return nums[n-2];
+
+    }
+}
