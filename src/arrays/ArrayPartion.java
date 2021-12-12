3f726b3f99154b26474d9286a976f067b5efc2a0
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-12 21:22:11 +0100

    impl arrayPartion

---
+++ src/arrays/ArrayPartion.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayPartion {
+    public static int arrayPartion(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length -1; i+= 2) {
+            sum +=nums[i];
+        }
+       return sum;
+    }
+}
