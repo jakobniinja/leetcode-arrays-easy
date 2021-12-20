e9bb564441ecc7d41541b6d750d6851fe3317447
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-20 17:14:11 +0100

    impl  sortArrayByPairity (proud)
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/arrays/SortArrayByPairity.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SortArrayByPairity {
+    public static int[] sortArrayByPairity(int[] nums) {
+
+        int n = nums.length - 1;
+        int[] res = new int[n+1];
+        int count = 0;
+
+        for (int i = 0; i <= n; i++) {
+            if (nums[i] % 2 == 0) res[count++] = nums[i];
+            else res[n - i] = nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        System.out.print(Arrays.toString(sortArrayByPairity(nums)));
+    }
+}
