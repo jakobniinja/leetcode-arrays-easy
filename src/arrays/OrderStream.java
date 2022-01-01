f294a2de547b58cc8b708a572b7f48c116ea8f1d
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-01 20:40:02 +0100

    impl OrderStream
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/arrays/OrderStream.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderStream {
+    Map<Integer, String> map;
+    int cap, idx;
+    public  OrderStream(int n ) {
+        map = new HashMap<>();
+        cap =n ;
+        idx =1;
+    }
+
+    public List<String> insert(int idKey, String value){
+        if(idKey > cap  || idKey< 0) return new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        map.put(idKey, value);
+        if(idKey === idx){
+            while(map.containsKey(idx)){
+                res.add(map.get(idx));
+                idx++;
+            }
+        }
+        return res;
+    }
+}
