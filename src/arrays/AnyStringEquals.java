7e88ee988a46396cd8b53abf107bb2bc0d194ab5
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-01 20:51:19 +0100

    impl elegant anyStringEquals2
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

--- src/arrays/AnyStringEquals.java
+++ src/arrays/AnyStringEquals.java
@@ -8,11 +8,21 @@ public class AnyStringEquals {
         }
         return count== word1.length;
     }
+    public static  boolean anyStringEquals2(String[] word1, String[] word2) {
+        String str1="",str2="";
+        for(String s:word1) {
+            str1 += s;
+        }
+        for(String s:word2) {
+            str2 += s;
+        }
 
+        return str1.equals(str2);
+    }
 
     public static void main(String[] args) {
         String[] word1 ={"abc", "d", "defg"};
         String[] word2 ={"abcddefg"};
-        System.out.println(anyStringEquals(word1, word2));
+        System.out.println(anyStringEquals2(word1, word2));
     }
 }
