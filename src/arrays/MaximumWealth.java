54e73f9a9337cf81d14741f19a29046c2bf01c14
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-02 12:28:59 +0100

    impl maximumWealth
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/arrays/MaximumWealth.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class MaximumWealth {
+    public static int maximumWealth(int[][] grid) {
+        int max = Integer.MIN_VALUE;
+        for( int i =0; i < grid.length; i++){
+            int ans = 0;
+
+            for(int j = 0; j < grid[0].length; j++){
+                ans += grid[i][j];
+            }
+            if (ans >= max) {
+                max = ans;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid ={{2,8,7}, {7,1,3}, {1,9,5}};
+        System.out.println(maximumWealth(grid));
+    }
+}
