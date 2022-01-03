a266d85e21bc95b8134f072df79a2777648cc0ce
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-04 00:14:16 +0100

    impl maximumPopulation
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

--- src/arrays/MaximumPopulation.java
+++ src/arrays/MaximumPopulation.java
@@ -2,20 +2,25 @@ package arrays;
 
 public class MaximumPopulation {
     public static int  maximumPopulation(int[][] logs) {
-        final int startYr = 1950;
-        int[] pops = new int[101];
-        for (int i = 0; i < logs.length; i++) {
-            pops[logs[i][0] - startYr]++;
-            pops[logs[i][1] - startYr]--;
-        }
-        int maxPop = pops[0], res = startYr;
-        for (int i = 1; i < pops.length; i++) {
-            pops[i] += pops[i - 1];
-            if (pops[i] > maxPop) {
-                res = i + startYr;
-                maxPop = pops[i];
+        int maxPopulation = 0, minYear =0;
+        var pop = new int[101];
+
+        for(var log: logs){
+            for( int year =log[0]; year< log[1]; year ++){
+                var x = year - 1950;
+                if(++pop[x] > maxPopulation){
+                    maxPopulation = pop[x];
+                    minYear = year;
+                }else if (pop[x] == maxPopulation && year < minYear){
+                    minYear = year;
+                }
             }
         }
-        return res;
+        return minYear;
+    }
+
+    public static void main(String[] args) {
+        int[][] logs = {{1950, 1961}, {1960, 1971}, {1970, 1981}};
+        System.out.println(maximumPopulation(logs));
     }
 }
