efe4c888c4d77dc703f3a0480b054285b8487783
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-06 09:19:08 +0100

    impl kthDistinct
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/arrays/KthDistinct.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthDistinct {
+    public static String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s: arr){
+            map.put(s, map.getOrDefault(s,0) +1);
+        }
+        for(String s : arr ){
+            if(map.get(s) ==1){
+                k--;
+                if(k==0) return s;
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"d","b","c","b","c","a"};
+        System.out.println(kthDistinct(arr, 1));
+    }
+}
