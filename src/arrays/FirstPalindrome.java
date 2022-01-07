0ec772689ef289b3abed904386e5bcacc4b983c9
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-07 09:39:11 +0100

    impl elegant firstPalindrome2
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

--- src/arrays/FirstPalindrome.java
+++ src/arrays/FirstPalindrome.java
@@ -10,8 +10,22 @@ public class FirstPalindrome {
         return "";
     }
 
+    public static String firstPalindrome2(String[] words) {
+        for(String word: words){
+            if(isPalindrom(word)) return word;
+        }
+        return "";
+    }
+
+    private static boolean isPalindrom(String word) {
+        int l =0, r =word.length()-1;
+        while(l < r)
+            if(word.charAt(l++) != word.charAt(r--)) return false;
+            return true;
+    }
+
     public static void main(String[] args) {
-        String[] words ={"abc","car","jakob","racr","cool"};
-        System.out.println(firstPalindrome(words));
+        String[] words ={"abc","car","jakob","racar","cool"};
+        System.out.println(firstPalindrome2(words));
     }
 }
