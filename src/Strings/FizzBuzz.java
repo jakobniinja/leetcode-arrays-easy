62e54838a7864e361223f0303c7142721a14f382
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-09 22:14:45 +0100

    impl fizzBuzz
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/Strings/FizzBuzz.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static List<String> fizzBuzz(int n ) {
+       List<String> res = new ArrayList<>();
+       for(int i =1; i<=n; i++){
+           if ( i % 3 ==0 && i % 5 ==0) res.add("FizzBuzz");
+           else if (i % 5 ==0) res.add("Buzz");
+           else if (i % 3  ==0) res.add("Fizz");
+           else res.add(""+ i);
+       }
+       return res;
+    }
+
+    public static void main(String[] args) {
+        int n  = 15;
+        System.out.println(fizzBuzz(n));
+    }
+}
