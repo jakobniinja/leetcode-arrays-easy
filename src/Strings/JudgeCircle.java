750647d6410bdc0e94183642186f67d44b7b0bba
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-13 18:43:06 +0100

    impl  judgeCircle
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/Strings/JudgeCircle.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class JudgeCircle {
+    public static boolean judgeCircle(String moves) {
+       int leftright =0, updown=0;
+
+       for(int i =0; i< moves.length(); i++){
+           if(moves.charAt(i) =='U')updown++;
+           else if (moves.charAt(i)== 'D')updown--;
+           else if (moves.charAt(i) == 'R')leftright++;
+           else if(moves.charAt(i) == 'L')leftright--;
+       }
+       return (updown == 0 && leftright==0);
+    }
+
+    public static void main(String[] args) {
+        String s = "DDLUUURDD";
+        System.out.println(judgeCircle(s));
+    }
+}
