a3f6f8bc25332d938b624b88f6de837653f86b06
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-21 23:41:26 +0100

    impl removeGoodString
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/Strings/RemoveGoodString.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class RemoveGoodString {
+    public static String removeGoodString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 1; i < sb.length(); i++) {
+            if (Math.abs((int) sb.charAt(i) - (int) sb.charAt(i - 1)) == 32) {
+                sb.deleteCharAt(i - 1);
+                sb.deleteCharAt(i - 1);
+                i = 0;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "s";
+        System.out.println(removeGoodString(s));
+    }
+
+}
+
