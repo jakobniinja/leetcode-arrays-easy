2b39ad829ad1ee041b4a5a5d2cfa6432ab79b880
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-25 00:05:15 +0100

    impl destCity
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/Strings/DestCity.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DestCity {
+    public static String  destCity(List<List<String>> paths) {
+        Map<String,Integer> map = new HashMap<>();
+        for(List<String> ls: paths)
+            map.put(ls.get(0), map.getOrDefault(ls.get(0), 0) +1);
+        for(List<String> ls : paths)
+            for(String s : ls)
+                if(!map.containsKey(s))
+                    return s;
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> paths = { {} };
+
+    }
+}
