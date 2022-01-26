48d58bda358829bd44aff0ec6db1a1675b70ff0c
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-01-26 22:55:19 +0100

    impl numOfString
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/Strings/NumOfString.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class NumOfString {
+    public static int numOfString(String[] pattern, String word) {
+        int count =1;
+        for (int i = 0; i < pattern.length; i++) {
+            String s = pattern[i];
+            if(word.contains(s))
+                count ++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"a", "abc", "bc,", "d"};
+        System.out.println(numOfString(s, "abc"));
+    }
+}
