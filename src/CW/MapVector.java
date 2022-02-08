1c5a7e39a3fa2d7c33f5db92002da38e1bfc7799
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-02-08 19:47:43 +0100

    impl mapVector
    Signed-off-by: jakobniinja <jakob.yaro@utb.ecutbildning.se>

---
+++ src/CW/MapVector.java
@@ -0,0 +1,16 @@
+package CW;
+
+public class MapVector {
+    public static double[]  mapVector(double[] vector, double[] circle1, double[] circle2) {
+        double xRatio = (vector[0] + circle1[2] - circle1[0]) / (2 * circle1[2]);
+        double yRatio = (vector[1] + circle1[2] - circle1[1] ) / (2 * circle1[2]);
+        return new double[]{
+                circle2[0] - circle2[2] + 2*  circle2[2] * xRatio,
+                circle2[1] - circle2[2] + 2 * circle2[2] * yRatio
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mapVector(new double[] { }));
+    }
+}
