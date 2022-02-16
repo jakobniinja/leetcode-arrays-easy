b708457f14b1ed9186f2a6666164a87c4d0f2fc4
jakobniinja <jakobyaro127@hotmail.com>
2022-02-16 17:16:04 +0100

    impl lunarMath

--- src/CW/LunarMath.java
+++ src/CW/LunarMath.java
@@ -5,17 +5,17 @@ public class LunarMath {
 
         long t = 0, c =1;
 
-        while (n1 != 0 || n2 != 0){
-
+        while (n1 != 0 ||  n2!= 0){
+            t += Math.max(n1%10, n2%10) * c ;
+            n1/=10; n2/=10; c*=10;
         }
 
-
-        return 4L;
+        return t;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(lunarMath(124, 3));
+        System.out.println(lunarMath(124, 7));
     }
 
 }
