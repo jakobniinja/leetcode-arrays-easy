4fec14b015da58146774251b95a7d0d8da2b68af
jakobniinja <jakobyaro127@hotmail.com>
2022-02-16 17:10:53 +0100

    impl lunarMath

---
+++ src/CW/LunarMath.java
@@ -0,0 +1,21 @@
+package CW;
+
+public class LunarMath {
+    public static long lunarMath(long n1, long n2) {
+
+        long t = 0, c =1;
+
+        while (n1 != 0 || n2 != 0){
+
+        }
+
+
+        return 4L;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lunarMath(124, 3));
+    }
+
+}
