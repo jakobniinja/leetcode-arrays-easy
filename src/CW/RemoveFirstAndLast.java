545a5bede29275b560d250c704a2d006148ec20e
jakobniinja <jakobyaro127@hotmail.com>
2022-02-20 19:29:09 +0100

    impl removeFirstAndLast

---
+++ src/CW/RemoveFirstAndLast.java
@@ -0,0 +1,13 @@
+package CW;
+
+import java.util.stream.Stream;
+
+public class RemoveFirstAndLast {
+    public static String removeFirstAndLast(String str) {
+        return str.substring(1, str.length()-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeFirstAndLast("person"));
+    }
+}
