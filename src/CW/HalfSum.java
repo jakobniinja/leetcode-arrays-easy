15e4d0746633a8cd2db004fbc6b12674b41c3d8b
jakobniinja <jakobyaro127@hotmail.com>
2022-03-02 21:50:00 +0100

    impl halfSum

---
+++ src/CW/HalfSum.java
@@ -0,0 +1,23 @@
+package CW;
+
+import java.util.stream.IntStream;
+
+public class HalfSum {
+    public static int halfSum(int n) {
+
+        int sum =0;
+        while(n !=1){
+            sum +=n;
+            n/=2;
+        }
+
+        return sum+1;
+    }
+//    public static int halfSum2(int n){
+//        return IntStream.iterate(n, x -> x !=0, x -> x/2).sum();
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(halfSum2(25));
+    }
+}
