68f42d2142a23fbd1363dc58a4ac043c524206db
jakobniinja <jakobyaro127@hotmail.com>
2022-03-05 14:52:29 +0100

    impl countSheep declerative

--- src/CW/CountSheep.java
+++ src/CW/CountSheep.java
@@ -1,5 +1,9 @@
 package CW;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class CountSheep {
     public static String  countSheep(int n) {
         StringBuilder sb = new StringBuilder();
@@ -10,7 +14,16 @@ public class CountSheep {
         return sb.toString();
     }
 
+    public static String countSheep2(int n){
+         return IntStream.rangeClosed(1, n).mapToObj(i -> i + " sheep...")
+                 .collect(Collectors.joining());
+
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println(countSheep(5));
+        System.out.println(countSheep2(5));
+        System.out.println(countSheep2(100));
     }
 }
