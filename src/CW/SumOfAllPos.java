d71b599b0f13ea4d55720c9a835ddc88139eb111
jakobniinja <jakobyaro127@hotmail.com>
2022-03-05 15:11:54 +0100

    impl countSheep

--- src/CW/SumOfAllPos.java
+++ src/CW/SumOfAllPos.java
@@ -8,7 +8,9 @@ import java.util.stream.Stream;
 public class SumOfAllPos {
     public static int sumOfAllPos(int[] nums) {
         return   Arrays.stream(nums).filter(num -> num > 0).sum();
+
     }
+
 
     public static void main(String[] args) {
         int[] list = new int[]{-1,-2,5,-4,15};
