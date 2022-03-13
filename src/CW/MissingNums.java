65dcd7c48e69b5b8640ba322330dc247bf2eae58
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-03-13 22:16:20 +0100

    impl missingNumsg -wip

---
+++ src/CW/MissingNums.java
@@ -0,0 +1,37 @@
+package CW;
+
+import io.reactivex.internal.d.e.P;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MissingNums {
+    public static String missingNums(int[] arr) {
+
+
+        List<Integer> res2 = new ArrayList<>();
+
+
+        2IntStream.of(arr).map(i -> i.)
+
+        for (int i = 0; i < 10; i++) {
+            res2.add(i);
+        }
+        for (int el : arr){
+            System.out.println(el);
+        }
+        System.out.println(Arrays.toString(res2.toArray()));
+
+        System.out.println(s);
+        return "hi";
+    }
+
+    public static void main(String[] args) {
+        int[] list = new int[]{12,34,56,78};
+        System.out.println(missingNums(list));
+    }
+}
