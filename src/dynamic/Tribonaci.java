2c53185dfb507d5744315d2f7bee54df8fb19419
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-03-22 20:45:41 +0100

    impl  tribonac & mm

---
+++ src/dynamic/Tribonaci.java
@@ -0,0 +1,72 @@
+package dynamic;
+
+public class Tribonaci {
+    public static int tribonaci(int n) {
+        if (n<= 0) return 0;
+        int n0 =0, n1 = 1, n2=1;
+        for (int i = 3; i <= n; i++) {
+            int tmp = n2;
+            n2 = n0 + n1 +n2;
+            n0 = n1;
+            n1 = tmp;
+        }
+        return n2;
+    }
+
+
+    public static int tribonaci2(int n){
+        if(n<= 0){
+            return 0;
+        }else if(n ==1 | n== 2){
+            return 1;
+        }else{
+            return tribonaci(n-3) + tribonaci(n-2) + tribonaci(n-1);
+        }
+    }
+    public static int fibonaci(int n){
+        if(n ==0) return 0;
+        if(n ==1 || n ==0) return 1;
+        return fibonaci(n-2) + fibonaci(n-1);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static int fib( int n){
+        int n1 = 0, n2=1;
+        for (int i = 2; i <= n; i++) {
+            int tmp = n1 + n2;
+            n1 =n2;
+            n2 = tmp;
+        }
+        return n2;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+    }
+}
